641572a85b8c7603f536ffd3dc4a25aa4a85b8a8
q-lee1001 <quintinlee@live.ca>
2025-01-23 22:50:32 -0800

    B: Drew triangle on screen and added circle class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pygame, constants
+import pygame, constants, player
 from constants import *
+from player import *
 def main():
 
 	pygame.init()
@@ -7,14 +8,24 @@ def main():
 Screen width: 1280
 Screen height: 720""")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+	clock = pygame.time.Clock()
+	dt = 0
+	x= SCREEN_WIDTH/2
+	y= SCREEN_HEIGHT/2
+	player = Player(x, y)
 	while True:
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0,0,0))
+
+		player.draw(screen)
+
+
 		pygame.display.flip()
+		
+		dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
